7b53c7e595ec87d11945cc3f55da0a5c71fe7cc7
Jonathan Hong <jonhong777@gmail.com>
2018-08-10 16:51:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,17 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      found_item = @@items.find{|element| element.name == item_name}
+        item_name = req.path.split("/items/").last
+        found_item = @@items.find{|element| element.name == item_name}
+        if found_item == nil
+          resp.write "We don't have this item"
+          resp.status = 400
+        else
+
+
+        end 
     else
-      resp.status = 404
+        resp.status = 404
     end
   end
 end
