96a7068ca2bc43a4e3c1f1b0c93e7197d440ba36
Jonathan Hong <jonhong777@gmail.com>
2018-08-10 16:54:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
         item_name = req.path.split("/items/").last
         found_item = @@items.find{|element| element.name == item_name}
         if found_item == nil
-          resp.write "We don't have this item"
+          resp.write "Item not found"
           resp.status = 400
         else
           resp.write "#{found_item.price}\n"
