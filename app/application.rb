cc2e11b8e61a71f6856325705e9049f1aa00cb31
Jonathan Hong <jonhong777@gmail.com>
2018-08-10 16:42:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,5 +2,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+    else
+      resp.status = 404
+    end
   end
 end
